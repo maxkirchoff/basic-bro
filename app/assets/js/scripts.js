6d4d7d35b2c13d98b916eac6083fc77aa17b7279
Max Kirchoff <max@lamasix.com>
2017-11-13 11:14:00 -0500

    initial commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * basic-bro
+ * Just your basic HTML/SASS/JS bro.
+ * https://github.com/LamaSix/basic-bro
+ * @author Max Kirchoff
+ * @version 1.0.0
+ * Copyright 2017. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // App
+  });
+
+})(jQuery, window, document);
